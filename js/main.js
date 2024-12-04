82ff611dea23ce8d542ee1d5410ef747d23a55b1
****** <*******.com>
2024-12-04 12:31:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Change button to active when clicked
+// $(".category-button").click(function() {
+//   $(".category-button").removeClass("active");
+//   $(this).addClass("active");
+// });
+
+
+// Makes variables for button and page content 
+var $categoryButton = $(".category-button");
+var $pageContent = $(".page-content");
+
+// Hide all page content shows first one
+$(".page-content")
+  .hide()
+  .first()
+  .show();
+
+// When button is clicked, show content 
+$categoryButton.on("click", function(e) {
+var $category = $(this).data("target");
+  $pageContent
+    .hide()
+    .find('img').hide()
+    .end()
+    .filter("." + $category)
+    .show()
+    .find('img').fadeIn();
+});
+
+
+
+(function($) {
+  $('.nav_modal .li_modal').click(function() {
+    $(this).addClass('.active').siblings('.li_modal').removeClass('.active');
+    $('.section_modal:nth-of-type('+$(this).data('rel')+')').stop().fadeIn(400, 'linear').siblings('.section_modal').stop().fadeOut(400, 'linear'); 
+  });
+})(jQuery);
+
+
+
+$(function(){
+  $('#datepicker').datepicker();
+});
